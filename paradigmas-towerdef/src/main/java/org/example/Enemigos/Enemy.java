09ae3b1de695654bec8163bac420f93fe0dc2c25
AdrianoSantino <adrianosantinof@gmail.com>
2024-09-09 22:51:56 -0300

    Class enemy cleaned.

--- paradigmas-towerdef/src/main/java/org/example/Enemigos/Enemy.java
+++ paradigmas-towerdef/src/main/java/org/example/Enemigos/Enemy.java
@@ -1,58 +1,27 @@
 package org.example.Enemigos;
 
-import org.example.Mapa.MapElements.MapElement;
-
-
-import org.example.Mapa.MapElements.CerroDeLaGloria;
-import org.example.Mapa.MapElements.MapElement;
 import org.example.Mapa.MapElements.pathCell;
 
 public abstract class Enemy {
 
-    protected int health;
-    protected boolean debuffState;
-    protected int gold;
-    protected int magic;
-    protected int damage;
-    protected int walkrate;
-    protected int range;
-    protected pathCell cell;
+    pathCell cell;
+    int health;
+    int gold;
+    int magic;
+    int damage;
+    int walkRate;
+    int range;
+    boolean debuffState;
 
-    public Enemy(int health, int gold, int magic, int damage, int walkrate, int range) {
+    public Enemy(int health, int gold, int magic, int damage, int walkRate, int range) {
         this.health = health;
         this.gold = gold;
         this.magic = magic;
         this.damage = damage;
-        this.walkRate = walkrate;
+        this.walkRate = walkRate;
         this.range = range;
         this.debuffState = false;
     }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public int getGold() {
-        return gold;
-    }
-
-    public int getMagic() {
-        return magic;
-    }
-
-    public int getDamage() {
-        return damage;
-    }
-
-
-    public float getWalkrate() {
-        return walkrate;
-    }
-
-    public int getRange() {
-        return range;
-    }
-
 
     public pathCell getCell() {
         return cell;
@@ -60,44 +29,32 @@ public abstract class Enemy {
 
     public void setDebuff(boolean state) {
         this.debuffState = state;
-        if (state) {
-            if (this.walkrate > 1) {
-                this.walkrate = (int) this.walkrate / 2;
-            }
-        } else {
-            this.walkrate = (int) this.walkrate * 2;
-        }
+        if (state)
+            if (this.walkRate > 1)
+                this.walkRate = (int) this.walkRate / 2;
+            else
+                this.walkRate = (int) this.walkRate * 2;
     }
 
     public boolean isAlive() {
-
         return this.health > 0;
     }
 
     public void walk(Enemy enemy) {
-        int i;
-        for (i = 0; i == this.walkrate; i++) {
+        for (int i = 0; i == this.walkRate; i++) {
             if (enemy.cell.next != null) {
                 enemy.cell.enemies.remove(enemy);
                 enemy.cell = enemy.cell.next;
                 enemy.cell.addEnemy(enemy);
-        }
-        enemy.setDebuff(false);
-    }
-
-    public void attackCerro(Enemy enemy, CerroDeLaGloria cerro) {
-        if (enemy.cell.next instanceof MapElement.CerroDeLaGloria) {
-            cerro.health = cerro.health - enemy.damage;
+            }
+            enemy.setDebuff(false);
         }
     }
-
-    public int getRow() {
-        return this.cell != null ? this.cell.row : -1;
-    }
-
-    public int getCol() {
-        return this.cell != null ? this.cell.col : -1;
-    }
-
+        /*
+        public void attackCerro (Enemy enemy, CerroDeLaGloria cerro){
+            if (enemy.cell.next instanceof MapElement.CerroDeLaGloria) {
+                cerro.health = cerro.health - enemy.damage;
+            }*/
 }
+
 
--- paradigmas-towerdef/src/main/java/org/example/Enemigos/EnemyDwarf.java
+++ paradigmas-towerdef/src/main/java/org/example/Enemigos/EnemyDwarf.java
@@ -1,28 +1,23 @@
 package org.example.Enemigos;
 
 
-public class EnemyDwarf  extends Enemy {
-    private int baseHealth;
-    private int baseDamage;
-    private int baseSpeed;
+public class EnemyDwarf extends Enemy {
+    int baseHealth;
+    int baseDamage;
+    int baseSpeed;
+
     public EnemyDwarf() {
-
         super(70, 20, 10, 7, 1, 1);
-        baseHealth = 70;
-        baseDamage = 20;
-        baseSpeed = 1;
-
+        this.baseHealth = 70;
+        this.baseDamage = 20;
+        this.baseSpeed = 1;
     }
 
     public void dwarfBuff(EnemyDwarf dwarf) {
-        float buff= (float) dwarf.baseHealth /2;
-
-        if (dwarf.health>=buff){
-            dwarf.damage=dwarf.baseDamage*2;
-            dwarf.walkrate=dwarf.baseSpeed*2;
+        float buff = (float) dwarf.baseHealth / 2;
+        if (dwarf.health >= buff) {
+            dwarf.damage = dwarf.baseDamage * 2;
+            dwarf.walkRate = dwarf.baseSpeed * 2;
         }
-
     }
-
-
 }
