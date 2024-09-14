974eb6ce95f1bb6ec7135e67bc98c54193bc644a
lucianapuentes <lucianapb80@gmail.com>
2024-09-14 11:47:18 -0300

    añadir metodos de dwarf y elf

--- src/main/java/org/example/Enemigos/EnemyDwarf.java
+++ src/main/java/org/example/Enemigos/EnemyDwarf.java
@@ -3,12 +3,40 @@ package org.example.Enemigos;
 
 import org.example.Map.Map;
 
+/**
+ * Especialización de la clase Enemy
+ * @author Luciana Puentes
+ * @version 1.3
+ *
+ */
+
 public class EnemyDwarf extends Enemy {
     public int baseHealth;
     public int baseDamage;
-    public int baseSpeed;
 
     public EnemyDwarf(Map map) {
         super(map, 100, 66, 10, 1);
+        this.baseHealth = 100;
+        this.baseDamage = 10;
     }
+
+    /**
+     * Método que duplica el daño base del enemigo tipo enano
+     * si éste tiene más de la mitad de la vida
+     * @
+     */
+    public void buffDwarf(){
+       int buff=(int) (this.baseHealth/2);
+        if (this.health>=buff){
+            this.damage=this.baseDamage*2;
+
+        }
+    }
+    public void debuffDwarf(){
+        int debuff=(int) (this.baseHealth/2);
+        if (this.health<=debuff){
+            this.damage=this.baseDamage;
+        }
+    }
+
 }
--- src/main/java/org/example/Enemigos/EnemyElf.java
+++ src/main/java/org/example/Enemigos/EnemyElf.java
@@ -1,11 +1,29 @@
 package org.example.Enemigos;
 
 import org.example.Map.Map;
+import org.example.Map.MapElements.CDLGloria;
+import org.example.Map.MapElements.Path;
 
 public class EnemyElf extends Enemy {
 
     public EnemyElf(Map map) {
         super(map, 100, 66, 10, 5);
+    }
+    @Override
+    public boolean nextIsCerro() {
+        Path nextnextPath= this.path.next.next;
+        Path nextPath = this.path.next;
+        int nextPathRow = nextPath.getRow();
+        int nextPathCol = nextPath.getCol();
+        int nextnextPathRow = nextnextPath.getRow();
+        int nextnextPathCol = nextnextPath.getCol();
+        return map.getGrid()[nextPathRow][nextPathCol] == map.getEndCell() || map.getGrid()[nextnextPathRow][nextnextPathCol] == map.getEndCell();
+    }
+    public void controller(Enemy elf) {
+        if (!nextIsCerro())
+            walk(elf);
+        else
+            attackCerro();
     }
 }
 
