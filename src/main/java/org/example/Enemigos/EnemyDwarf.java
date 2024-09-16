a9484e3f8ba86a3639b42572e317c2cd2ff17c47
lucianapuentes <162376975+lucianapuentes@users.noreply.github.com>
2024-09-15 23:11:34 -0300

    comentarios java doc y gigaDwarf

--- src/main/java/org/example/Enemigos/EnemyDwarf.java
+++ src/main/java/org/example/Enemigos/EnemyDwarf.java
@@ -2,13 +2,38 @@ package org.example.Enemigos;
 
 
 import org.example.Map.Map;
-
+/**
+ * especialización de Enemy para el modelado de enemigo de tipo enano
+ * @author Luciana Puentes
+ */
 public class EnemyDwarf extends Enemy {
     public int baseHealth;
     public int baseDamage;
-    public int baseSpeed;
-
+    /**
+     * Crea un enemigo de tipo enano 
+     * @param map el maoa donde se crea el enemigo de tipo enano
+     */
     public EnemyDwarf(Map map) {
+        /**
+         * @see Enemy
+         */
         super(map, 100, 66, 10, 1);
     }
+    /**
+     * método que aumenta el daño que inflinge el enano
+     * si se encuentra a más de la mitad de la vida
+     */
+    public void gigaDwarf() {
+        /**
+         * si la vida actual del enano es maypr a la mitad de su vida base,
+         * establece su daño como el doble del daño base
+         */
+        if (health > (int)baseHealth / 2) {
+            damage = baseDamage * 2;}
+        else{
+            damage = baseDamage;
+        }
+
+
+        }
 }
