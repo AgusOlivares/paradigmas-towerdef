02c7d3f742808a4a23436c6e7399403c3ab718c4
lucianapuentes <162376975+lucianapuentes@users.noreply.github.com>
2024-09-15 22:54:24 -0300

    comentarios java doc

--- src/main/java/org/example/Map/MapElements/Tower.java
+++ src/main/java/org/example/Map/MapElements/Tower.java
@@ -5,7 +5,10 @@ import org.example.Map.Cell;
 import org.example.Map.Map;
 
 import java.util.ArrayList;
-
+/**
+ * Especialización de la clase map element para modelar las torres
+ * @author Adriano Fabris
+ */
 public class Tower extends MapElement {
     public Map map;
     public ArrayList<Path> atkCells;
@@ -17,15 +20,27 @@ public class Tower extends MapElement {
 
     public Tower(int row, int col, Map map) {
         super(row, col);
+        /**
+         * @see MapElement
+         */
         this.map = map;
         this.atkCells = this.findAtkCells();
         this.enemyQueue = new ArrayList<>();
     }
-
+    /**
+     * Método para infligir daño a los enemigos
+     */
     // Ataca al enemigo en la cola ordenada
     public void attack() {
+        /**
+         * Recorre la cola de enemigos de la torre y ataca al enemigo en la cabeza de la cola.
+         * Si el enemigo muere, lo quita de la cola de enemigos
+         */
         if (!this.enemyQueue.isEmpty()) {
             Enemy enemy = this.enemyQueue.get(0);
+            /**
+             * @see Enemy
+             */
             enemy.receiveDamage(this.damage);
             if (!enemy.isAlive()) {
                 this.enemyQueue.remove(enemy);
@@ -34,10 +49,20 @@ public class Tower extends MapElement {
     }
 
     // Actualiza la cola ordenada de enemigos
+    /**
+     * Método para actualizar la cola de ataque de la torre
+     */
     public void updateEnemyOrder() {
+        /**
+         * verifica una por una las posiciones atacables de la torre em caso de que
+         * haya llegado un nuevo enemmigo, si es así lo añade a la cola de ataque
+         */
         enemyQueue = new ArrayList<>();
         for (Path path : this.atkCells) {
             for (Enemy enemy : path.getEnemies()) {
+                /**
+                 * @see Path
+                 */
                 if (enemy != null && !this.enemyQueue.contains(enemy)) {
                     this.enemyQueue.add(enemy);
                 }
@@ -46,7 +71,16 @@ public class Tower extends MapElement {
     }
 
     // Encuentra las celdas (que pertenenecen al camino enemigo) a las que la torre puede atacar
+    /**
+     * Método que encuentra las celdas del camino enemigo a las que la torre puede atacar
+     * @return atkCells una lista que contiene las celdas que la torre puede atacar
+     */
     public ArrayList<Path> findAtkCells() {
+        /**
+         * recorre el rango de celdas que la torre puede atacar (calculado mediante range)
+         * encontrando aquellas que son instancias de Path
+         * si lo son, las celdas son añadidas a la lista atkCells
+         */
         ArrayList<Path> atkCells = new ArrayList<>();
 
         int minRow = this.getRow() - this.range;
@@ -70,7 +104,17 @@ public class Tower extends MapElement {
     }
 
     // Checkea rápidamente si un enemigo está en el rango de la torre
+    /**
+     * Método que verifica si un enemigo se encuentra dentro del rango de la torre
+     * @param enemy el emenigo a verificar
+     * @return true si el enemigo está dentro del rango de la torre, false en caso contrario
+     */
     public boolean insideRange(Enemy enemy) {
+        /**
+         * compara los valores row,col del enemigo al rango de la torre
+         * si los valores del enemigo pertenecen al intervalo que define dicho rango,
+         * el enemigo está dentro del rango de la torre
+         */
         int minRow = this.getRow() - this.range;
         int maxRow = this.getRow() + this.range;
         int minCol = this.getCol() - this.range;
