11370d295061d0625d217d19a62a8ce8bfaa3a4c
lucianapuentes <162376975+lucianapuentes@users.noreply.github.com>
2024-09-15 23:08:23 -0300

    comentarios java doc

--- src/main/java/org/example/Map/MapElements/CDLGloria.java
+++ src/main/java/org/example/Map/MapElements/CDLGloria.java
@@ -1,11 +1,23 @@
 package org.example.Map.MapElements;
+/**
+ * Especialización de la clase map element para modelar el cerro de la gloria
+ * @author Agustín Olivares
+ */
 
 public class CDLGloria extends MapElement {
 
     public int health;
-
+    /**
+     * Crea una instancia de CDLGloria a partir de una posición de fila, otra de columna y una cantidad de puntos de vida
+     * @param row posición de fila
+     * @param col posición de columna
+     * @param health cantidad de puntos de vida
+     */
     public CDLGloria(int row, int col, int health) {
         super(row, col);
+        /**
+         * @see MapELement
+         */
         this.health = health;
     }
 
@@ -16,8 +28,15 @@ public class CDLGloria extends MapElement {
     public void setHealth(int health) {
         this.health = health;
     }
-
+    /**
+     * Método que verifica si el Cerro de la Gloria ha caído
+     * @return true si el Cerro ha caído, false en caso contrario
+     */
+     
     public boolean isDeath(){
+        /**
+         * Si la cantidad de puntos de vida es menor o igual a cero retorna true
+         */
         return health <= 0;
     }
 }
