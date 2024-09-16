966dbd44228d1bc8e907eb60ddc6151febd95fe2
lucianapuentes <lucianapb80@gmail.com>
2024-09-16 09:23:52 -0300

    attack de SpecialTower

--- src/main/java/org/example/Map/MapElements/SpecialTower.java
+++ src/main/java/org/example/Map/MapElements/SpecialTower.java
@@ -1,14 +1,46 @@
 package org.example.Map.MapElements;
 
+import org.example.Enemigos.Enemy;
 import org.example.Map.Map;
 
+/**
+ * Especialización de la clase tower para torres con habilidades especiales
+ * @author Adriano Fabris
+ */
 public class SpecialTower extends Tower {
 
     public int damage = 1000;
     public int cost = 200;
     public int range = 3;
 
+    /**
+     * Crea una torre de tipo especial
+     * @param row fila donde se posiciona la torre
+     * @param col columna donde se posiciona la torre
+     * @param map mapa donde se posiciona la torre
+     */
     public SpecialTower(int row, int col, Map map) {
+        /**
+         * @see Tower
+         */
         super(row, col, map);
+    }
+    @Override
+    /**
+     * Ataca al enemigo, reduciendo su vida y ralentizándolo
+     */
+    public void attack() {
+        if (!this.enemyQueue.isEmpty()) {
+            //si la torre tiene enemigos en su cola de ataque
+            Enemy enemy = this.enemyQueue.get(0); //ataca al primer enemigo
+            /**
+             * @see Enemy
+             */
+            enemy.receiveDamage(this.damage); //reduce la vida del enemigo
+            enemy.setDebuff(true); //ralentizar al enemigo
+            if (!enemy.isAlive()) { //si el enemigo murió
+                this.enemyQueue.remove(enemy); //eliminar al enemigo de la cola de ataque
+            }
+        }
     }
 }
