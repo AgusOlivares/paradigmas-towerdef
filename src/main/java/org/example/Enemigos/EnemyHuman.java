6a6b5036f2a4e0218e6f1c98e3ce0e168497d784
lucianapuentes <162376975+lucianapuentes@users.noreply.github.com>
2024-09-15 23:18:14 -0300

    comentarios java doc

--- src/main/java/org/example/Enemigos/EnemyHuman.java
+++ src/main/java/org/example/Enemigos/EnemyHuman.java
@@ -1,9 +1,15 @@
 package org.example.Enemigos;
 
 import org.example.Map.Map;
-
+/**
+ * Especialización de la clase Enemy para el enemigo de tipo humano
+ * @author Luciana Puentes
+ */
 public class EnemyHuman extends Enemy {
-
+    /**
+     * Crea un enemigo de tipo humano
+     * @param map mapa donde se crea el enemigo de tipo humano
+     */
     public EnemyHuman(Map map) {
         super(map, 100, 66, 10, 2);
     }
