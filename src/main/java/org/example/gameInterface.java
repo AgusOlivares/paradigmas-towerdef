6e944776af984428eec514f9c11cdb2f08e31866
Agustin Olivares <faolivares3112@gmail.com>
2024-09-17 02:08:29 -0300

    Generacion de JavaDoc

--- src/main/java/org/example/gameInterface.java
+++ src/main/java/org/example/gameInterface.java
@@ -1,11 +1,23 @@
 package org.example;
 
+/**
+ * Interfaz para opciones generales de juego
+ */
 public interface gameInterface {
 
+    /**
+     * Muestra detalles simples con respecto al Juego
+     */
     public void ShowInstructions();
 
+    /**
+     * Muestra detalles simples con respecto a la informacion del jugador
+     */
     public void ShowPlayerDetails();
 
+    /**
+     * Muestra detalles simples con respecto a la tematica general del juego
+     */
     public void ShowIntroduction();
 
 }
